a1371d273093123fe4c87dbfb58dd578c47d3370
Tino Schüttpelz <tino.schuettpelz@icomps.de>
2021-08-18 15:02:09 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RiderBug2021._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Example {
+        private Payload _value;
+
+        public Payload Value {
+            get => _value is DerivedPayload ? throw new InvalidOperationException() : _value;
+            set => _value = value;
+        }
+    }
+
+    class Payload {
+
+    }
+
+    class DerivedPayload : Payload {
+
+    }
+}
